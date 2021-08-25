c66c78540c047523e49215045620425523657fcc
Washington Crisóstomo <washington.parra@gmail.com>
2021-08-25 08:38:09 -0300

    Basic structure and navbar

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+<footer class="navbar-default navbar-fixed-bottom">
+    <div class="container-fluid">
+        <span>WASHINGTON PARRA CRISOSTOMO
+            20008425-5
+            CURSO
+            SUPERIOR DE TECNOLOGIA EM SISTEMAS PARA INTERNET
+        </span>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ pages/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doces da Terezinha</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-sandstone.css" />
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Doces da Terezinha</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Início <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Localização</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
